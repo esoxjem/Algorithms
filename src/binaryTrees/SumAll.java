d20ae091b932c48b322d2290ddc8a439412a1b19
Arun Sasidharan <arun91.exe@gmail.com>
2013-10-31 15:10:51 +0530

    BT: sum - non recursive

--- src/binaryTrees/SumAll.java
+++ src/binaryTrees/SumAll.java
@@ -1,11 +1,19 @@
 package binaryTrees;
 
 public class SumAll {
+
+	// recursive
 	public int sumOfAllElements(BinaryTreeNode root) {
-		if(root == null){
+		if (root == null) {
 			return 0;
 		}
-		return (root.getData() + sumOfAllElements(root.getLeft()) + sumOfAllElements(root.getRight()));
-		
+		return (root.getData() + sumOfAllElements(root.getLeft()) + sumOfAllElements(root
+				.getRight()));
+
 	}
+
+	/**
+	 * For noon recursive, do a level order traversal and keep adding
+	 * node.getData()
+	 */
 }
