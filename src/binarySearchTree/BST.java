cec586cf0371ea29301af25da5045d6882213abf
Arun Sasidharan <arun91.exe@gmail.com>
2013-11-02 03:15:03 +0530

    more op

--- src/binarySearchTree/BST.java
+++ src/binarySearchTree/BST.java
@@ -78,4 +78,37 @@ public class BST {
 		}
 		return root;
 	}
+
+	// insert node recursively
+	BSTNode insert(BSTNode root, int data) {
+		if (root == null) {
+			root = new BSTNode(data);
+		}
+		if (data < root.getData()) {
+			root.setLeft((insert(root.getLeft(), data)));
+		} else if (data > root.getData()) {
+			root.setRight(insert(root.getRight(), data));
+		}
+		return root;
+	}
+
+	// insert iterative
+	void insertIterative(BSTNode root, int data) {
+		if (root == null) {
+			root = new BSTNode(data);
+		} else {
+			while (root.getLeft() != null && root.getRight() != null) {
+				if (data < root.getData()) {
+					root = root.getLeft();
+				} else if (data > root.getData()) {
+					root = root.getRight();
+				}
+			}
+			if (data < root.getData()) {
+				root.setLeft(new BSTNode(data));
+			} else if (data > root.getData()) {
+				root.setRight(new BSTNode(data));
+			}
+		}
+	}
 }
