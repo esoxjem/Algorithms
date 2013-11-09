bf06f27854dd4436e134446439f7c1d4e39b4d8a
Arun Sasidharan <arun91.exe@gmail.com>
2013-11-09 16:25:39 +0530

    added selection sort

--- src/sorting/Test.java
+++ src/sorting/Test.java
@@ -3,9 +3,9 @@ package sorting;
 public class Test {
 
 	public static void main(String[] args) {
-		BubbleSort b = new BubbleSort();
+		SelectionSort s = new SelectionSort();
 		int[] arr = {4, 5, 1, 3, 2};
-		b.bubbleSort(arr);
+		s.selectionSort(arr);
 		for(int i : arr){
 			System.out.print(i + " ");
 		}
