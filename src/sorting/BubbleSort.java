cd88d0e8e02b8460865145781d0c095a24181c17
Arun Sasidharan <arun91.exe@gmail.com>
2013-11-09 12:08:19 +0530

    added improved bubble sort

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -2,15 +2,34 @@ package sorting;
 
 public class BubbleSort {
 
+	// bubble sort in O(n2)
 	void bubbleSort(int arr[]) {
 		int size = arr.length;
-		for (int pass = size - 1; pass >= 0; pass--) { //in every pass largest element moves to the last
+		for (int pass = size - 1; pass >= 0; pass--) {
 			for (int j = 0; j < pass; j++) {
 				if (arr[j] > arr[j + 1]) {
-					//swap elements
+					// swap elements
 					int temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	// bubble sort, best case in O(n)
+	void bubbleSortImproved(int arr[]) {
+		int temp, size = arr.length;
+		boolean swapped = true;
+		for (int pass = size - 1; pass >= 0 && swapped; pass--) {
+			swapped = false;
+			for (int j = 0; j < pass; j++) {
+				if (arr[j] > arr[j + 1]) {
+					// swap elements
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+					swapped = true;
 				}
 			}
 		}
