180b0cf2bc2878a44163e5134670778cf31d4158
Arun Sasidharan <arun91.exe@gmail.com>
2013-11-11 01:10:13 +0530

    added merge sort

--- src/sorting/Test.java
+++ src/sorting/Test.java
@@ -3,9 +3,9 @@ package sorting;
 public class Test {
 
 	public static void main(String[] args) {
-		InsertionSort is = new InsertionSort();
-		int[] arr = {1, 5, 4, 3, 2};
-		is.insertionSort(arr);
+		MergeSort ms = new MergeSort();
+		int[] arr = {1, 5, 4, 3, 2, 6};
+		ms.mergeSort(arr);
 		for(int i : arr){
 			System.out.print(i + " ");
 		}
