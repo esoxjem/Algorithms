8a103edba12315e08cf7e94a7928d217d197e559
Arun Sasidharan <arun91.exe@gmail.com>
2013-11-21 11:09:09 +0530

    added heap

---
+++ src/heaps/Heap.java
@@ -0,0 +1,44 @@
+package heaps;
+
+public class Heap {
+
+	int capacity; // size
+	int heap_arr[];
+	int count; // number of elements
+
+	public Heap(int capacity, int heap_type) {
+		this.capacity = capacity;
+		heap_arr = new int[capacity];
+	}
+
+	int parent(int i) {
+		if (i > 0 && i < count) {
+			return (i - 1) / 2;
+		}
+		return -1;
+	}
+
+	int leftChild(int i) {
+		int left = i * 2 + 1;
+		if (left > count) {
+			return -1;
+		}
+		return left;
+	}
+
+	int rightChild(int i) {
+		int right = i * 2 + 2;
+		if (right > count) {
+			return -1;
+		}
+		return right;
+	}
+	
+	int getMax(){
+		if(count == 0)
+			return -1;
+		else
+			return heap_arr[0];
+	}
+
+}
