9ffd1a28ec805b307fe771aba9a10d2f82c7409c
Arun Sasidharan <arun91.exe@gmail.com>
2014-01-21 22:52:14 +0530

    Fixed Josephus circle implementation

--- src/circularLinkedLists/JosephusCircle.java
+++ src/circularLinkedLists/JosephusCircle.java
@@ -4,26 +4,28 @@ package circularLinkedLists;
  * */
 public class JosephusCircle {
 
-	// elect leader by eliminating every Mth person from the circle
-	public void getJosephusPosition(int n, int m) {
-
-		CLLNode current = new CLLNode(1);
-		CLLNode head = current;
-
-		// create CLL
-		for (int i = 2; i <= n; ++i) {
-			CLLNode newNode = new CLLNode(i);
-			current.setNext(newNode);	
+	// elect leader by eliminating every Kth person from the circle
+	LLNode josephusPosition(LLNode head, int k)
+	{
+		LLNode prev = null, current = head, next = null;
+		k--;
+	
+		while(current != current.next)
+		{	
+			int count = k;
+			while(count!=0)
+			{
+				prev = current;
+				current = current.next;
+				count--;
+			}		
+					
+			next = current.next;
+			prev.next = next;
+			current.next = null;
+			current = next;	
 		}
-		current.setNext(head);
-
-		// eliminate every Mth player as long as > 1 remains
-		for(int count = n; count > 1; --count){
-			for(int i = 0; i < m - 1; i++)
-				head = head.getNext();  // move to m-1
-			head.setNext(head.getNext().getNext()); //set m-1 to m+1, eliminating m
-		}
-		System.out.println(head.getData());
-
+		
+		return current;
 	}
 }
