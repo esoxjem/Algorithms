23e84efd2fc1af70b7d3dcde12672fc6edb0d55a
Arun Sasidharan <arunsasidharan121@gmail.com>
2020-04-10 16:52:36 +0200

    if to when

--- src/main/kotlin/algos/arrays/BinarySearch.kt
+++ src/main/kotlin/algos/arrays/BinarySearch.kt
@@ -7,37 +7,28 @@ class BinarySearch {
     }
 
     private fun search(arr: Array<Int>, start: Int, end: Int, target: Int): Int {
-        val mid = (start + end) / 2
-
         if (start > end) {
             return -1
         }
+        val mid = (start + end) / 2
 
-        if (arr[mid] == target) {
-            return mid
-        }
-
-        return if (mid < target) {
-            search(arr, mid + 1, end, target)
-        } else {
-            search(arr, start, mid - 1, target)
+        return when {
+            arr[mid] == target -> mid
+            mid < target -> search(arr, mid + 1, end, target)
+            else -> search(arr, start, mid - 1, target)
         }
     }
 
     fun searchIterative(arr: Array<Int>, target: Int): Int {
         var left = 0
         var right = arr.lastIndex
-        
+
         while (left <= right) {
             val mid = (left + right) / 2
-            if (arr[mid] == target) {
-                return mid
-            }
-
-            if (arr[mid] < target) {
-                left = mid + 1
-            } else {
-                right = mid - 1
+            when {
+                arr[mid] == target -> return mid
+                arr[mid] < target -> left = mid + 1
+                else -> right = mid - 1
             }
         }
 
@@ -49,7 +40,7 @@ fun main(args: Array<String>) {
     val arr = arrayOf(1, 2, 4, 10, 22)
 
     val bs = BinarySearch()
-    println("index: " + bs.search(arr, 2))
+    println("index: " + bs.search(arr, 10))
     println("index: " + bs.search(arr, 12))
 
     println("index: " + bs.searchIterative(arr, 22))
